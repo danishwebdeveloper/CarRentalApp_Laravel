0363d6db724580b2d0b816ebc999eadad06f9c3e
MuhammadDanish <danishbutt.uos@gmail.com>
2022-02-06 22:02:43 +0100

    Check testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,11 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+    // A basic test for the Git test
     public function test_example()
     {
         $this->assertTrue(true);
